a014ec9786dd14bb35aa1deab9979f5c69526f3e
KumalyakovaAS <explay1355@mail.ru>
2023-03-07 17:02:59 +0300

    e5

---
+++ e5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if (username.ToLower() =="сашка")
+{
+    Console.WriteLine("Ура, это же Сашка!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
