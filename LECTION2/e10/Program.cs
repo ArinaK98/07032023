c40448b05514da6415b9dbf83585238cb61c4057
KumalyakovaAS <explay1355@mail.ru>
2023-03-11 15:19:52 +0300

    Лекция 2

---
+++ LECTION2/e10/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {11,21,31,44,18,65,7,18,39};
+
+int n = array.Length;
+int find = 18;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
